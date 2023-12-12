3300e52a1a8d999c82508518e03d9bfc654a5398
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2023-12-12 12:09:22 -0500

    Update errors.lua

--- errors.lua
+++ errors.lua
@@ -6,7 +6,7 @@ function __Orb.ThrowError(type,file,line)
     local line = line or nil 
     local types = {
         ["Complier"] = "Working on it!",
-        ["Not_found"] = "Orb: error\ntraceback\n\t[orbit]: <"..file.."> file not found\n\t[file]: "..currentFile.."\n\t[line]: "..tostring(line)
+        ["Not_found"] = "Orb: error\ntraceback\n\t[orb]: <"..file.."> file not found\n\t[file]: "..currentFile.."\n\t[line]: "..tostring(line)
     }
     if line == nil then
         types["Not_found"] = "Orb: error\ntraceback\n\t[orbit]: missing input file"
