0d60415fc1ee49a1bc23439e5cf82444a162c985
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2023-12-12 12:08:51 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,15 @@ currentFile = "main"
 local pathToFile = {"main"}
 local isString = {isString = false, stringSE = "NULL"}
 lexer.lex("main")
-for _,i in spairs(tokenTable) do
+for _,i in spairs(syntax) do
+  print("Line: ".._.." = {")
+  for k,v in spairs(tokenTable[_]) do
+    print(k,v[1],v[2])
+  end
+  print("}\n")
+end
+
+--[[for _,i in spairs(tokenTable) do
   for k,v in spairs(i) do
     if isString.isString and v[1] ~= assigned_Token[isString.stringSE] then
       v[1] = "KTOKEN_TYPE_STRING"
@@ -25,4 +33,9 @@ for _,i in spairs(tokenTable) do
     print("String Token: "..assigned_Token[isString.stringSE])
     print("Item: "..v[2].."\nLine: ".._.."\nPosition: "..k.."\nToken: "..v[1].."\n\nString: "..tostring(isString.isString))
   end
-end
+end]]
+
+--[[testStr = 'if ([typeof($words)] != "string"):{'
+for _,i in pairs(testStr:position("string")) do
+  print(_,i)
+end]]
