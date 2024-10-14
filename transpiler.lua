788b95fe5c5205a61982213f7f1d67c3cd3b6cd8
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2024-10-13 22:55:44 -0400

    Update transpiler.lua

--- transpiler.lua
+++ transpiler.lua
@@ -3,8 +3,10 @@ local Tokens = require("Tokens")
 
 local function autofill(t,value)
     for _,i in pairs(t) do
-        if _:lower():find(value:lower()) then
-            return _
+        if not tonumber(_) then
+            if _:lower():find(value:lower()) then
+                return _
+            end
         end
     end
 end
@@ -44,6 +46,7 @@ function transpiler.translate() -- No input need because im super smart >:)
                     table.remove(Statements,#Statements)
                     t[2] = "end"
                 end
+                if t[3] ~= nil and t[3]:find("COMMENT") then goto isComment end
                 if t[1]:find("OBRACE") and fullTokens[_][s-1][1]:find("COLON") and #Statements > 0 then
                     if Statements[#Statements][1]:lower():find("func") then
                         t[2] = nil
@@ -57,19 +60,32 @@ function transpiler.translate() -- No input need because im super smart >:)
                 if t[2] == Tokens.OTOKEN_KEYWORD_PUTLN():lower() then
                     t[2] = "print("
                     fullTokens[_][#fullTokens[_]][2] = ");"
+                elseif t[2] == Tokens.OTOKEN_KEYWORD_PUT():lower() then
+                    t[2] = "io.write("
+                    fullTokens[_][#fullTokens[_]][2] = ");"
                 elseif t[2] == Tokens.OTOKEN_KEYWORD_FUNCTION():lower() then
                     t[2] = "function "
                 elseif t[2] == Tokens.OTOKEN_KEYWORD_SET():lower() then
                     t[2] = nil
                 elseif t[2] == Tokens.OTOKEN_KEYWORD_STATIC():lower() then
-                    t[2] = "static"
+                    t[2] = "local"
                 elseif t[2] == Tokens.OTOKEN_TYPE_COLON() and not t[1]:find("STRING") then
                     if fullTokens[_][s+1][2] == "Number" or fullTokens[_][s+1][2] == "String" or fullTokens[_][s+1][2] == "Array" or fullTokens[_][s+1][2] == "Any" or fullTokens[_][s+1][2] == "Char" or fullTokens[_][s+1][2] == "Bool" then
                         t[2] = nil
+                        fullTokens[_][s+1][1] = "OTOKEN_SPECIAL_TYPE"
                         fullTokens[_][s+1][2] = nil
                     end
+                elseif t[1]:find("CONCAT") then
+                    t[2] = ".."
+                elseif t[1]:find("NOT_EQUALTO") then
+                    t[2] = "~="
+                elseif t[1]:find("KEY_OR") then
+                    t[2] = "or"
+                elseif t[1]:find("KEY_AND") then
+                    t[2] = "and"
                 end
                 translated[_][#translated[_]+1] = t[2]
+                ::isComment::
             end
             ::passThrough::
         end
