4632807c039b1eb60cc0d070e46346d40afe2b97
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2025-05-19 22:39:41 -0400

    Update builder.lua

--- src/Xohe/builder.lua
+++ src/Xohe/builder.lua
@@ -2,12 +2,13 @@ local builder = {}
 
 --[[Imports]]--
 local Lexer = require("src/lexer")
-local Functions = require("src/Functions")
+local Parser = require("src/parser")
+local Functions = require("src/functions")
 
 local function_data = {
     put = {
         "global func put(words) {",
-        "print(words)",
+        "XOHE.PUSH_OP.WRITE(words)",
         "ret words",
         "}"
     },
@@ -20,5 +21,6 @@ builder.builtins.put = {
     Value = Functions.getValue("put",Lexer.lex(function_data.put,true),1).Value,
     Content = Functions.getValue("put",Lexer.lex(function_data.put,true),1).Contents
 }
+
 
 return builder
