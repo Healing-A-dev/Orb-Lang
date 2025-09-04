36fd1a9fba0a1704db4501c2b1d3129a0dae2b45
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2025-09-03 23:20:59 -0400

    Add files via upload

--- lib/asm/std.s
+++ lib/asm/std.s
@@ -1,1 +1,40 @@
+.section .data
+	int_buffer:
+		.skip 128
+	
+.section .text
+	.global Orb_CFUNCTION_puts
+	.global Orb_CFUNCTION_exit
+	.global Orb_CFUNCTION_puts_INT
 
+	Orb_CFUNCTION_puts:
+	    mov $1, %rax
+	    mov $1, %rdi
+	    syscall
+	    ret
+	
+	Orb_CFUNCTION_exit:
+	    mov $60, %rax
+	    syscall
+	    ret
+	
+	Orb_CFUNCTION_puts_INT:
+	    mov %rax, %rdi
+	    mov $int_buffer + 32, %rsi
+	    mov $10, %rcx
+	
+	.displayInt:
+	    xor %rdx, %rdx
+	    div %rcx
+	    add $'0', %dl
+	    dec %rsi
+	    mov %dl, (%rsi)
+	    test %rax, %rax
+	    jnz .displayInt
+	
+	    mov $int_buffer + 32, %rdx
+	    sub %rsi, %rdx
+	    mov %rsi, %rsi
+	    callq "Orb_CFUNCTION_puts"
+	    ret
+	
